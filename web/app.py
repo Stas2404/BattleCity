6f2b65b955326a3ef7f1c457447446b689ee72df
Stas2404 <stanislavyudin24@gmail.com>
2025-12-24 19:26:13 +0000

    Додано код гри та вебсайту

---
+++ web/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, send_from_directory
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/download')
+def download_game():
+    return send_from_directory('static', 'game.zip', as_attachment=True)
+
+if __name__ == '__main__':
+    app.run(debug=True)
